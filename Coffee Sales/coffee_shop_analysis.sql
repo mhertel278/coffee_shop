517e91851d99b617b5fd56aa5e7b8912f86d2778
unknown <mhertel278@gmail.com>
2023-01-30 00:05:29 -0600

    did lots of stuff

--- Coffee Sales/coffee_shop_analysis.sql
+++ Coffee Sales/coffee_shop_analysis.sql
@@ -1,3 +1,52 @@
+/* Exploratory Analysis and Data Cleaning
+*/
+
+--examine duplicate transaction_ids
+-- examine rows where transaction_id is duped
+with row_counter as (
+SELECT * , COUNT(*) OVER (PARTITION BY transaction_id) as row_count
+FROM transactions
+ORDER BY 1,2,4)
+
+SELECT * FROM row_counter
+WHERE row_count > 1
+;
+
+
+/*
+High Level Analysis
+*/
+-- Top line totals
+SELECT COUNT(DISTINCT CONCAT(transaction_id, transaction_date, sales_outlet_id)) as total_transactions
+	, SUM (line_item_amount) as dollars_sold_tot
+	, SUM (quantity) as units_sold_total
+	, SUM (line_item_amount - unit_price) as profit_dollars_total
+FROM transactions
+;
+--Top line by store
+SELECT sales_outlet_id
+	, COUNT(DISTINCT CONCAT(transaction_id, transaction_date, sales_outlet_id)) as total_transactions
+	, SUM (line_item_amount) as dollars_sold_tot
+	, SUM (quantity) as units_sold_total
+	, SUM (line_item_amount - unit_price) as profit_dollars_total
+FROM transactions
+GROUP BY 1
+ORDER BY 1
+;
+
+--Top Line by product category ranked
+SELECT p.product_category
+	, COUNT(DISTINCT CONCAT(t.transaction_id, t.transaction_date, t.sales_outlet_id)) as total_transactions
+	, SUM (t.line_item_amount) as dollars_sold_tot
+	, SUM (t.quantity) as units_sold_total
+	, SUM (t.line_item_amount - unit_price) as profit_dollars_total
+FROM transactions t
+JOIN product p
+ON t.product_id = p.product_id
+GROUP BY 1
+ORDER BY 3 DESC
+;
+
 /*
 grouping transactions by store and product group to join to
 unpivoted sales goals and calculate the actual vs goal
@@ -24,3 +73,40 @@ ORDER BY 1,2
 
 ;
 
+/* 
+Analyze pastry waste
+*/
+
+-- see if additional pastries get made throughout the day or if start num is total daily inv
+
+SELECT *
+FROM pastry_inventory
+WHERE start_of_day != (quantity_sold + waste)
+;
+
+-- find overall waste perc by product
+
+SELECT i.product_id
+	, p.product
+	, ROUND(SUM(CAST(i.quantity_sold as  NUMERIC))
+		/SUM(CAST(i.quantity_sold as NUMERIC) + CAST(i.waste as NUMERIC)) 
+			* 100, 2) as waste_perc
+FROM pastry_inventory i
+JOIN product p
+ON i.product_id = p.product_id
+GROUP BY 1, 2
+ORDER BY 3 ASC
+;
+
+-- find waste by store
+SELECT sales_outlet_id
+	, ROUND(SUM(CAST(quantity_sold as  NUMERIC))
+		/SUM(CAST(quantity_sold as NUMERIC) + CAST(waste as NUMERIC))
+			 * 100, 2) as waste_perc
+FROM pastry_inventory 
+
+GROUP BY 1
+ORDER BY 2 ASC
+;
+
+-- find waste perc by week for store 3 to see if promo reduced overall waste
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # coffee_shop
+
+## EDA and Data Cleaning as necessary
+
+- Check for nulls in any table
+
+- Check for duplicates, either complete row dups or dups of specific IDs
+
+### Transactions Table
+
+    - Duplicate transaction_ids. Each store on each day seems to start new count. Also same transaction can have multiple line items, one for each different product regardless of how many units of the product (ie can have 2 coffees on one line but not 1 coffee and one tea)
+
+    - no dups
+
+### 
+
+    
+
+## High Level Analysis
+
+- Find total # of transactions, total units sold, total $ sold, total profit
+
+- Find above totals by store
+
+- 
+
