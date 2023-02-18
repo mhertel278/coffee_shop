b6d2ba932c2f73fe3eb7d278e30be61350496d8b
unknown <mhertel278@gmail.com>
2023-02-17 23:46:48 -0600

    began customer analysis

---
+++ Coffee Sales/coffee_shop_customer_analysis.sql
@@ -0,0 +1,31 @@
+-- find how many transactions are from loyalty vs non-loyalty members
+SELECT COUNT(DISTINCT CONCAT(transaction_date,transaction_time,sales_outlet_id)) as trans_count
+	, 'non-loyalty' as customer_type
+FROM transactions
+WHERE customer_id NOT IN (SELECT customer_id from customer)
+UNION 
+SELECT COUNT(DISTINCT CONCAT(transaction_date,transaction_time,sales_outlet_id)) as trans_count
+	, 'loyalty' as customer_type
+FROM transactions
+WHERE customer_id IN (SELECT customer_id from customer)
+
+-- how many new loyalty customers this month
+SELECT COUNT(*) 
+FROM CUSTOMER
+WHERE customer_since > '2019-04-01'
+
+-- money spent by gender
+SELECT c.gender
+	, SUM(t.line_item_amount) as dollars_spent	
+FROM customer c
+INNER JOIN transactions t
+ON c.customer_id = t.customer_id
+GROUP BY 1
+
+--money spent per transaction by gender
+SELECT c.gender
+	, ROUND(SUM(t.line_item_amount)/ COUNT(DISTINCT CONCAT(t.transaction_id,t.transaction_date,t.sales_outlet_id)),2) as dollars_per_trans
+FROM customer c
+INNER JOIN transactions t
+ON c.customer_id = t.customer_id
+GROUP BY 1
